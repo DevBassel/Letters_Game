eebdda4266ec283f7100eee5ffdfab5b35e49ef8
DevBassel <bassel444555@gmail.com>
2023-03-05 21:18:53 +0200

    update

--- app/App.js
+++ app/App.js
@@ -1,37 +1,105 @@
-let startBtn = document.querySelector(".start");
+let startBtns = document.querySelectorAll(".control button");
 let borde = document.querySelector(".game-borde");
-let ltrs = "abcdefghijklmnopqrstuvwxyz".split("");
+let ltrs_en = "abcdefghijklmnopqrstuvwxyz".split("");
+let ltrs_ar = [
+  "ا",
+  "ب",
+  "ت",
+  "ث",
+  "ج",
+  "ح",
+  "خ",
+  "د",
+  "ذ",
+  "ر",
+  "ص",
+  "ض",
+  "ط",
+  "ظ",
+  "ع",
+  "غ",
+  "ف",
+  "ق",
+  "ل",
+  "م",
+  "ن",
+  "و",
+  "ه",
+  "ي",
+];
 let livesEl = document.querySelector(".lives");
 let scoreArea = document.querySelector(".score span");
 let current = [];
 let lives = 3;
+let genTime = 1300;
 let score = 0;
-let speed = 100;
+let speed = 110;
 let speedCount = 0;
 let dropInterval;
-livesEl.textContent = lives;
-scoreArea.textContent = score;
-startBtn.addEventListener("click", () => {
-  document.querySelector(".control").remove();
+let gen;
 
-  let Add = setInterval(() => randLtr(ltrs), 1200);
-
-  dropFun(Add);
+startBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let Add;
+    document.querySelector(".control").remove();
+    console.log(btn.className);
+    if (btn.className === "start_En") {
+      Add = setInterval(() => generat(ltrs_en), genTime);
+    } else {
+      Add = setInterval(() => generat(ltrs_ar), genTime);
+    }
+    dropFun(Add);
+  });
 });
 
-function randLtr(arr) {
+window.addEventListener("keydown", ({ key }) => {
+  let don = false;
+  let it = "";
+  current.forEach((item) => {
+    if (item.ltr === key) {
+      it = item;
+      don = true;
+      score += 10;
+      speedCount += 10;
+      scoreArea.textContent = score;
+      render();
+    }
+  });
+  if (it) {
+    fire(it);
+    setTimeout(() => (current = current.filter((el) => el.id !== it.id)), 330);
+  }
+  if (!don) {
+    lives -= 1;
+  }
+});
+
+document.querySelector(".restart").addEventListener("click", () => {
+  window.location.reload();
+});
+
+// function randLtr(arr) {}
+function generat(arr) {
+  clearInterval(gen);
+  let l = arr[Math.floor(Math.random() * arr.length)];
+
   current = [
     ...current,
     {
-      ltr: arr[Math.floor(Math.random() * ltrs.length)],
-      left: Math.floor(Math.random() * (borde.offsetWidth - 85)),
+      id: Date.now().toString(32),
+      ltr: l,
+      left: Math.floor(Math.random() * (borde.offsetWidth - 80)),
       top: 0,
     },
   ];
+
+  gen = setInterval(() => generat(), genTime);
 }
 function render() {
-  let html = "";
+  livesEl.textContent = lives;
+  scoreArea.textContent = score;
 
+  let html = "";
   current.forEach((item) => {
     html += `
     <div class="word" style="top: ${item.top}px; left: ${item.left}px">  
@@ -70,7 +138,7 @@ function dropFun(Add) {
   render();
 
   current.forEach((el) => {
-    if (el.top === 500) {
+    if (el.top === borde.parentElement.offsetHeight - 100) {
       lives -= 1;
       livesEl.textContent = lives;
     }
@@ -82,6 +150,7 @@ function dropFun(Add) {
 
     if (speed > 10) {
       speed -= 5;
+      genTime -= 100;
     }
   }
   if (lives === 0) {
@@ -89,19 +158,26 @@ function dropFun(Add) {
   } else dropInterval = setInterval(dropFun, speed);
 }
 
-window.addEventListener("keydown", ({ key }) => {
-  current.forEach((item) => {
-    if (item.ltr === key) {
-      let index = current.indexOf(item);
-      current.splice(index, 1);
-      score += 10;
-      speedCount += 10;
-      scoreArea.textContent = score;
-      render();
-    }
-  });
-});
+function fire(el) {
+  let cannon = document.querySelector(".cannon-body");
 
-document.querySelector(".restart").addEventListener("click", () => {
-  window.location.reload();
-});
+  let bullt = document.createElement("span");
+  bullt.className = "bullt-area";
+  document.querySelector(".area").prepend(bullt);
+  let height = borde.parentElement.offsetHeight - el.top - 50;
+  bullt.style.setProperty("height", `${height}px`);
+  bullt.style.setProperty("left", `${el.left + 40}px`);
+  let span = document.createElement("span");
+  span.className = "bullt";
+
+  bullt.appendChild(span);
+  cannon.classList.add("fire");
+  setTimeout(() => {
+    bullt.style.setProperty("height", `${35}px`);
+    bullt.style.setProperty("left", `${50}%`);
+    bullt.remove();
+    cannon.classList.remove("fire");
+  }, 300);
+
+  console.log(el.left);
+}
